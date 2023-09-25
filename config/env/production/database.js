a788d1692446d89a862e5a1c51dbbf483b6d38b3
Kin Wong <beewong2014@hotmail.com>
2023-09-25 17:08:02 +0100

    revert

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -15,25 +15,3 @@ module.exports = ({ env }) => ({
       debug: false,
     },
   });
-
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-module.exports = () => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-    debug: false,
-  },
-});
